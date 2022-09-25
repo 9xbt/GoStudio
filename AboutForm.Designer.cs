71646b8ed0b43ff706156c3912cb8a200087f6ab
9xbt <flippy632@gmail.com>
2022-09-25 17:59:33 +0200

    Update AboutForm.Designer.cs

--- AboutForm.Designer.cs
+++ AboutForm.Designer.cs
@@ -59,9 +59,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(50, 31);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(143, 13);
+            this.label2.Size = new System.Drawing.Size(112, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Made by MrDumbrava#9459";
+            this.label2.Text = "Made by MrDumbrava";
             // 
             // button1
             // 
